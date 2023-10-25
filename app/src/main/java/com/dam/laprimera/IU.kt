32b91585bfc4b32d52cc381bdc0f4a974a88c461
AngiCasella <acasella@danielcastelao.org>
2023-10-25 10:53:26 +0200

    Data Class actualizada

--- app/src/main/java/com/dam/laprimera/IU.kt
+++ app/src/main/java/com/dam/laprimera/IU.kt
@@ -19,72 +19,56 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.dam.laprimera.ui.theme.LaPrimeraTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun InterfazUsuario(miViewModel: MyViewModel){
-    //para que sea mas facil la etiqueta del log
-    val TAG_LOG:String="Mensaje UI"
+fun InterfazUsuario(miViewModel: MyViewModel) {
+    val TAG_LOG: String = "Mensaje UI"
 
-    //un cuadro de texto para mostrar los numeros
     Text(
-        text="Numeros: ${miViewModel.appState.numbers}",
-        modifier = Modifier
-            .offset(y = 160.dp)
+        text = "Numeros: ${miViewModel.appState.numbers}",
+        modifier = Modifier.offset(y = 160.dp)
     )
 
-    // un boton para generar numeros aleatorios
     Button(
         onClick = {
-            Log.d(TAG_LOG,"Dentro del onClick")
-            miViewModel.crearRandom()},
+            Log.d(TAG_LOG, "Dentro del onClick")
+            miViewModel.crearRandom()
+        },
         modifier = Modifier
-            .padding(vertical = 300.dp, horizontal = 100.dp) // Ajusta el espacio vertical según tus necesidades
-            .offset(y=100.dp)
+            .padding(vertical = 300.dp, horizontal = 100.dp)
+            .offset(y = 100.dp)
     ) {
         Image(
             painter = painterResource(id = R.drawable.monkey_emojis_4),
             contentDescription = "Generar numeros",
             modifier = Modifier
-                .size(50.dp) // Ajusta el tamaño de la imagen según tus necesidades
+                .size(50.dp)
                 .padding(4.dp)
         )
         Text(text = "Generar numeros")
     }
 
-    Login(miViewModel = MyViewModel())
-
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun Login(miViewModel: MyViewModel) {
     Column {
-        // mostrar el contador de clics
-        TextButton(onClick = { miViewModel.contadorClic() }, modifier= Modifier.offset(y=5.dp)) {
+        TextButton(onClick = { miViewModel.contadorClic() }, modifier = Modifier.offset(y = 5.dp)) {
             Text("CLICS: ${miViewModel.appState.counter}")
         }
 
-        // mientras no tecleamos mas de tres caracteres no se muestra el saludo
-        if (miViewModel.name.value.length > 3) {
+        if (miViewModel.appState.name.length > 3) {
             Text(
                 text = "Nombre: ${miViewModel.appState.name}!",
                 fontSize = 24.sp,
-                modifier = Modifier.offset(y=110.dp)
+                modifier = Modifier.offset(y = 110.dp)
             )
         }
-        // campo de texto para rellenar
+
         OutlinedTextField(
-            value = miViewModel.getTexto(),
-            onValueChange = {
-                miViewModel.name.value= it
-            },
-            label = { Text(text = "Name") }, modifier = Modifier.offset(y=5.dp)
+            value = miViewModel.appState.name,
+            onValueChange = { miViewModel.setTexto(it) },
+            label = { Text(text = "Name") },
+            modifier = Modifier.offset(y = 5.dp)
         )
-
     }
-
 }
-
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
--- app/src/main/java/com/dam/laprimera/MyViewModel.kt
+++ app/src/main/java/com/dam/laprimera/MyViewModel.kt
@@ -11,75 +11,27 @@ import androidx.lifecycle.ViewModel
 /**
  * Inicializa y modifica los datos de la app
  */
-class MyViewModel() : ViewModel() {
-    //para que sea mas facil la etiqueta del log
+class MyViewModel : ViewModel() {
     private val TAG_LOG: String = "Mensaje ViewModel"
 
     // Utiliza la Data Class para mantener el estado de la aplicación
     var appState by mutableStateOf(AppState(emptyList(), 0, ""))
 
-    //esta va a ser nuestra lista para la secuencia random
-    //usamos mutable, ya que la queremos modificar y observar cuando cambia
-    var _numbers = mutableStateOf(0)
-
-    //crear una lista mutable
-    val numbers= mutableStateListOf<Int>()
-
-    // Dos maneras diferentes de definir un estado
-    // con el = necesitamos utilizar value
-    var counter=mutableStateOf(0)
-    var name =mutableStateOf("")
-
-    //inicializamos variables cuando instanciamos, éste es el constructor de la clase
     init {
-        //se usa el log para saber cuando se ejecuta la instacia de la clase
         Log.d(TAG_LOG, "Inicializamos ViewModel")
     }
 
-    /**
-     * Crear entero random
-     */
     fun crearRandom() {
-        val randomNumber=(0..10).random()
-        numbers.add(randomNumber)
-        Log.d(TAG_LOG,"Creamos random $randomNumber")
+        val randomNumber = (0..3).random()
+        val newNumbers = appState.numbers.toMutableList()
+        newNumbers.add(randomNumber)
+        Log.d(TAG_LOG, "Creamos random $randomNumber")
 
-        //Imprime la lista completa
-        for(numero in numbers){
-            Log.d(TAG_LOG,"Números aleatorio: $numero")
-        }
-        appState = appState.copy(numbers = numbers)
+        appState = appState.copy(numbers = newNumbers)
     }
 
-    /**
-     * Crear entero random
-     */
-    fun getNumero(): Int {
-        return _numbers.value
-    }
-
-    fun getListaRandom(): List<Int> {
-        return numbers.toList()
-    }
-    /**
-     * Contador de clic
-     */
-    fun contadorClic(){
+    fun contadorClic() {
         appState = appState.copy(counter = appState.counter + 1)
-    }
-
-    /**
-     * Crear contador
-     */
-    fun getContador():Int{
-        return counter.value
-    }
-
-    /**
-     * Obtener texto
-     */
-    fun getTexto():String{
-        return name.value
     }
 
     fun setTexto(text: String) {
