04861d3772522ca758139ad3a7c7fde547939b7a
AngiCasella <acasella@danielcastelao.org>
2023-10-25 10:35:56 +0200

    Nueva rama ViewModel

--- app/src/main/java/com/dam/laprimera/IU.kt
+++ app/src/main/java/com/dam/laprimera/IU.kt
@@ -2,31 +2,18 @@ package com.dam.laprimera
 
 import android.util.Log
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -96,44 +83,6 @@ fun Login(miViewModel: MyViewModel) {
     }
 
 }
-@Composable
-fun Greeting(NAME: String, modifier: Modifier = Modifier) {
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.End
-    ) {
-        Box {
-            Image(painter = painterResource(R.drawable.gatito), contentDescription = "icono")
-            Icon(
-                painter = painterResource(id = R.drawable.check),
-                contentDescription = "icono_check"
-            )
-        }
-        Column {
-
-            Text(
-                text = "Hola $NAME",
-                fontSize = 50.sp,
-                color = Color.Blue,
-                modifier = modifier
-            )
-                Text(
-                text = stringResource(R.string.saludo),
-                fontSize = 20.sp,
-                color = Color.Blue,
-                modifier = modifier
-
-            )
-
-            Button(onClick = { Log.d("no pienso calcular", "jorge feo!!!!") }) {
-                Text(text = "Click me!")
-            }
-
-        }
-
-    }
-
-}
 
 
 @Preview(showBackground = true)
@@ -141,6 +90,5 @@ fun Greeting(NAME: String, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     LaPrimeraTheme {
         InterfazUsuario(miViewModel = MyViewModel())
-        //Greeting("Android")
     }
 }
--- app/src/main/java/com/dam/laprimera/MainActivity.kt
+++ app/src/main/java/com/dam/laprimera/MainActivity.kt
@@ -1,46 +1,16 @@
 package com.dam.laprimera
 
-import android.graphics.drawable.Icon
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.dam.laprimera.ui.theme.LaPrimeraTheme
 
 val TAG: String = "Estado"
-val NAME: String = "Android"
 
 
 class MainActivity : ComponentActivity() {
@@ -62,22 +32,6 @@ class MainActivity : ComponentActivity() {
             }
         }
         Log.d(TAG, "onCreate")
-
-        /*calcular(a = 3, b = 5, fun() {
-            //val suma = numero1 + numero2
-            //Log.d("calcular", suma.toString())
-        })
-
-        /*
-        Esto es una función que ya tiene parámetros predefinidos 
-        y una termina con función vacía como parámetro 
-        
-        fun calcular(a: Int = 0, b: Int = 0, operacion: () -> Unit) {
-        operacion()
-         */
-        calcular {
-            Log.d("calcular", "yo no hago nada, solo LOG")
-        }*/
     }
 
     override fun onStart() {
@@ -104,16 +58,4 @@ class MainActivity : ComponentActivity() {
         super.onDestroy()
         Log.e(TAG, "onDestroy")
     }
-
-    /*fun calcular(a: Int = 0, b: Int = 0, operacion: () -> Unit) {
-        //val operacion = a + b
-        //Log.d("calcular", operacion.toString())
-        //operacion(a, b)
-        operacion()
-    }*/
 }
-
-// Declara tu función para generar números aleatorios
-/*fun generarNumeroAleatorio(): Int {
-    return (0..10).random()
-}*/
